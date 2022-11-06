7cb9174bcae0e2169bbc09b15b8d938a4fad20b6
Jinge Zhang <jinge_windows@hotmail.com>
2022-11-06 12:30:52 +1100

    Update Cargo class

--- SailingAndTrading/Cargo.cs
+++ SailingAndTrading/Cargo.cs
@@ -8,6 +8,7 @@ namespace SailingAndTrading
 {
     public class Cargo
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public int SingleSpace { get; set; }
         public int Number { get; set; }
@@ -22,6 +23,15 @@ namespace SailingAndTrading
         }
         //此种商品的原产地，一种商品，离原产地越远，价格越高。
         public Island SourceIsland { get; set; }
+        public int Island_Id { get; set; }
+
+        public Cargo(int Id,string name, int singleSpace, int number,int Island_Id)
+        {
+            this.Name = name;
+            this.SingleSpace = singleSpace;
+            this.Number = number;
+        }
+
         public Cargo(string name, int singleSpace, int number)
         {
             this.Name = name;
